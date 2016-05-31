cec7eddbcab03a52c6bb692e135c86fa52b003c4
smileclj <842705148@qq.com>
2016-05-31 16:11:55 +0800

    x

---
+++ src/main/java/com/clj/panda/jar/util/DateUtil.java
@@ -0,0 +1,16 @@
+package com.clj.panda.jar.util;
+
+public class DateUtil {
+	/**
+	 * 你好吗
+	 * @param p 参数1
+	 * @param a 参数2
+	 * @return
+	 * long
+	 * @author chenlj
+	 * @version 2016 下午2:53:29
+	 */
+	public static long now(String p,Integer a) {
+		return System.currentTimeMillis();
+	}
+}
